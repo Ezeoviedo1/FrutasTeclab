5f32ce3e803990c6eb7ad77fd474ebd089011d8a
Ezeoviedo1 <136611384+Ezeoviedo1@users.noreply.github.com>
2023-06-23 11:03:13 -0300

    .

---
+++ Proyecto Base - TECLAB/js/index.js
@@ -0,0 +1,25 @@
+let nombre = "Teclab";
+let FechaNacimiento= "21/03/1975";
+let anioinicial = "2018";
+if(anioinicial === "2023"){
+    console.log("la variable anioInicial tiene como valor 2018")
+} else{
+    console.warn("El valor de anioInicial no es el esperado")
+}
+
+
+
+const usuario = "Joe McMillan";
+const securityId ='01216318578-4645-a454654';
+
+const paisesDelSur = ['Argentina','Uruguay','Venezuela','Chile'];
+
+const carrito =[{codigo:123, nombre: 'Teclado Bluetooth', importe: 15500},
+                {codigo:234, nombre: 'Mouse Bluetooth', importe: 7800},
+                {codigo:345, nombre: 'SSD Portatil', importe: 47350}]; 
+
+                let numerico ="1"
+                let string = "frutas"
+                const arrayFrutas = ["Manzana","Banana","Pera","Limon","Mango" ]
+                
+                           
